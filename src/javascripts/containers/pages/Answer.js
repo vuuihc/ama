288858eecde303593eb92b661279f2242668ff8b
武文齐 <wuwenqi@neotel.com.cn>
2016-06-21 16:07:49 +0800

    pages

---
+++ src/javascripts/containers/pages/Answer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import ''
+
+class Answer extends Component {
+
+    render(){
+        return (
+            <div className="accountAnswer">
+                <div className="question">
+                    <div className="head">
+                        <img src={require('../../../images/head.jpg')} style={{width:100, height:100}} />
+                        <span className="name">奥特曼</span>
+                        <span className="price">￥ 5.2</span>
+                    </div>
+                    <div className="stem">陈老师，长得太帅总是被爱怎么办，有么有什么解决办法</div>
+                    <div className="time">15分钟之前</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Answer;
