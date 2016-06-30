ae560119a1a738ffda32e16cdfe03711a29aa58d
wuwq <wuwenqi@neotel.com.cn>
2016-06-30 23:03:19 +0800

    我问的好了

---
+++ src/javascripts/containers/blocks/QuestionItemWithoutAvatarNotAnswered.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zhushihao on 2016/6/16.
+ */
+/**
+ * create by wuwq
+ */
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+import '../../../stylesheets/partials/modules/QuestionItemWithoutAvatarNotAnswered.scss';
+
+export default class QuestionItemWithoutAvatar extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {question} = this.props
+    return (
+      <article className="question-without-avatar-not-answered">
+        <div className="question-content">
+          <h4>{question.content}</h4>
+        </div>
+        <div className="status"><div className="btn">待解决</div></div>
+      </article>
+    )
+  }
+}
