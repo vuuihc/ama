c309370516ea1dd750fe16d6f4022d6bda47608a
vuuihc <zshyou@gmail.com>
2016-07-03 15:26:48 +0800

    联调支付

--- src/javascripts/containers/TutorIndex.js
+++ src/javascripts/containers/TutorIndex.js
@@ -22,13 +22,13 @@ class TutorIndex extends Component {
     const time = new Date()
     if(nextProps.prepayInfo.timeStamp!=undefined && time.valueOf()/1000-nextProps.prepayInfo.timeStamp<5){
       console.log("获得了最新的timestamp")
-      console.log(nextProps.prepayInfo.data)
+      console.log(nextProps.prepayInfo)
       wx.chooseWXPay({
-        timestamp:nextProps.prepayInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-        nonceStr: nextProps.prepayInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
-        package: nextProps.prepayInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-        signType: nextProps.prepayInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-        paySign: nextProps.prepayInfo.data.paySign, // 支付签名
+        timestamp:nextProps.prepayInfo.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+        nonceStr: nextProps.prepayInfo.nonceStr, // 支付签名随机串，不长于 32 位
+        package: nextProps.prepayInfo.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+        signType: nextProps.prepayInfo.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+        paySign: nextProps.prepayInfo.paySign, // 支付签名
         success: function (res) {
           console.log("支付成功！");
         }
