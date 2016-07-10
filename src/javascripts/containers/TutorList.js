73cd814ba652291cf2dca33ef748b6d85f89f345
vuuihc <zshyou@gmail.com>
2016-07-10 11:36:34 +0800

    支

--- src/javascripts/containers/TutorList.js
+++ src/javascripts/containers/TutorList.js
@@ -36,7 +36,7 @@ class TutorList extends Component {
       <main className="tutor-list">
         {
           tutorList.data.map((tutor, index) =>
-            <Link key={index} to={baseUrl+"/tutor/"+tutor.user_id}>
+            <Link key={index} to={baseUrl+"tutor/"+tutor.user_id}>
               <article>
                 <div className="tutor-info">
                   <img src={tutor.user_face}/>
