760d9e214566dc6639d65a53b1a004c61686efd2
vuuihc <zshyou@gmail.com>
2016-07-11 22:46:44 +0800

    支

--- src/javascripts/containers/HotQuestionList.js
+++ src/javascripts/containers/HotQuestionList.js
@@ -56,14 +56,7 @@ class HotQuestionList extends Component {
         console.log("进入微信支付")
         function onBridgeReady(){
           WeixinJSBridge.invoke(
-            'getBrandWCPayRequest', {
-              "appId" : "wxfbe89421611bdb2d",     //公众号名称，由商户传入
-              "timeStamp":nextProps.listenInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-              "nonceStr": nextProps.listenInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
-              "package": nextProps.listenInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-              "signType": nextProps.listenInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-              "paySign": nextProps.listenInfo.data.paySign, // 支付签名
-          },
+            'getBrandWCPayRequest', nextProps.listenInfo.data,
             function(res){
               if(res.err_msg == "get_brand_wcpay_request：ok" ) { console.log("支付成功！")}
               if( document.removeEventListener ){
