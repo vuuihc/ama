6f88151519b4c853bcbe21216d4616297915198d
vuuihc <zshyou@gmail.com>
2016-07-11 22:05:57 +0800

    支

--- src/javascripts/containers/Question.js
+++ src/javascripts/containers/Question.js
@@ -32,23 +32,59 @@ class Question extends Component {
       const time = new Date()
       if(time.valueOf()/1000-nextProps.listenInfo.data.timeStamp<5){
         console.log("进入微信支付")
-        wx.chooseWXPay({
-          timestamp:nextProps.listenInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-          nonceStr: nextProps.listenInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
-          package: nextProps.listenInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-          signType: nextProps.listenInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-          paySign: nextProps.listenInfo.data.paySign, // 支付签名
-          success: function (res) {
-            console.log("支付成功！");
-            self.state.listenTimer = setTimeout(() => self.props.dispatch(getListenInfo(answerId)),1000);
-            self.setState({playNow: false})
-          },
-          fail:function(res){
-            alert("支付失败")
-            console.log("失败原因：")
-            console.log(res)
+        function onBridgeReady(){
+          WeixinJSBridge.invoke(
+            'getBrandWCPayRequest', {
+              "appId" : "wxfbe89421611bdb2d",     //公众号名称，由商户传入
+              "timestamp":nextProps.listenInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+              "nonceStr": nextProps.listenInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
+              "package": nextProps.listenInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+              "signType": nextProps.listenInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+              "paySign": nextProps.listenInfo.data.paySign, // 支付签名
+            },
+            function(res){
+              if(res.err_msg == "get_brand_wcpay_request：ok" ) { 
+                console.log("支付成功！")
+              }else{
+                console.log(res)
+              }
+              if( document.removeEventListener ){
+                document.removeEventListener('WeixinJSBridgeReady', onBridgeReady);
+              }else if (document.attachEvent){
+                document.detachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.detachEvent('onWeixinJSBridgeReady', onBridgeReady);
+              }
+              // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+            }
+          );
+        }
+        if (typeof WeixinJSBridge == "undefined"){
+          if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+          }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
           }
-        });
+        }else{
+          onBridgeReady();
+        }
+        // wx.chooseWXPay({
+        //   timestamp:nextProps.listenInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+        //   nonceStr: nextProps.listenInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
+        //   package: nextProps.listenInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+        //   signType: nextProps.listenInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+        //   paySign: nextProps.listenInfo.data.paySign, // 支付签名
+        //   success: function (res) {
+        //     console.log("支付成功！");
+        //     self.state.listenTimer = setTimeout(() => self.props.dispatch(getListenInfo(answerId)),1000);
+        //     self.setState({playNow: false})
+        //   },
+        //   fail:function(res){
+        //     alert("支付失败")
+        //     console.log("失败原因：")
+        //     console.log(res)
+        //   }
+        // });
       }
     }else if(nextProps.listenInfo.data.url!=undefined){
       console.log("nextProps.listenInfo.data")
--- src/javascripts/containers/TutorIndex.js
+++ src/javascripts/containers/TutorIndex.js
@@ -27,25 +27,67 @@ class TutorIndex extends Component {
     if(nextProps.prepayInfo.data.timeStamp!=undefined && time.valueOf()/1000-nextProps.prepayInfo.data.timeStamp<5){
       console.log("获得了最新的timestamp")
       console.log(nextProps.prepayInfo)
-      wx.chooseWXPay({
-        timestamp:nextProps.prepayInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-        nonceStr: nextProps.prepayInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
-        package: nextProps.prepayInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-        signType: nextProps.prepayInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-        paySign: nextProps.prepayInfo.data.paySign, // 支付签名
-        success: function (res) {
-          console.log("支付成功！");
-          self.setState({askSuccess:true})
-          self.state.successTimer = setTimeout(()=>{
-            self.setState({askSuccess:false})
-            browserHistory.push(baseUrl+"account/IAskedList")
-          },2000)
-        },
-        fail:function(res){
-          console.log("失败原因：")
-          console.log(res)
+      function onBridgeReady(){
+        WeixinJSBridge.invoke(
+          'getBrandWCPayRequest', {
+            "appId" : "wxfbe89421611bdb2d",     //公众号名称，由商户传入
+            "timestamp":nextProps.prepayInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+            "nonceStr": nextProps.prepayInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
+            "package": nextProps.prepayInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+            "signType": nextProps.prepayInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+            "paySign": nextProps.prepayInfo.data.paySign, // 支付签名
+          },
+          function(res){
+            if(res.err_msg == "get_brand_wcpay_request：ok" ) { 
+              console.log("支付成功！")
+              if( document.removeEventListener ){
+                document.removeEventListener('WeixinJSBridgeReady', onBridgeReady);
+              }else if (document.attachEvent){
+                document.detachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.detachEvent('onWeixinJSBridgeReady', onBridgeReady);
+              }
+              self.setState({askSuccess:true})
+              self.state.successTimer = setTimeout(()=>{
+                self.setState({askSuccess:false})
+                browserHistory.push(baseUrl+"account/IAskedList")
+              },2000)
+            }else{
+              console.log(res)
+            }
+            
+            // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+          }
+        );
+      }
+      if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+          document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+        }else if (document.attachEvent){
+          document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+          document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
         }
-      });
+      }else{
+        onBridgeReady();
+      }
+      // wx.chooseWXPay({
+      //   timestamp:nextProps.prepayInfo.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+      //   nonceStr: nextProps.prepayInfo.data.nonceStr, // 支付签名随机串，不长于 32 位
+      //   package: nextProps.prepayInfo.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+      //   signType: nextProps.prepayInfo.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+      //   paySign: nextProps.prepayInfo.data.paySign, // 支付签名
+      //   success: function (res) {
+      //     console.log("支付成功！");
+      //     self.setState({askSuccess:true})
+      //     self.state.successTimer = setTimeout(()=>{
+      //       self.setState({askSuccess:false})
+      //       browserHistory.push(baseUrl+"account/IAskedList")
+      //     },2000)
+      //   },
+      //   fail:function(res){
+      //     console.log("失败原因：")
+      //     console.log(res)
+      //   }
+      // });
     }
   }
   getPrepayInfo(){
