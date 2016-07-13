96556373f31bb4b0a1880a3e11a907ea13b05a49
vuuihc <zshyou@gmail.com>
2016-07-13 23:45:26 +0800

    录音

--- src/javascripts/reducers/account/askedMe.js
+++ src/javascripts/reducers/account/askedMe.js
@@ -1,4 +1,4 @@
-import { RECEIVE_ASKED_ME, REQUEST_ASKED_ME, ASKED_ME_COMPLETED } from  '../../actions/ActionTypes'
+import { RECEIVE_ASKED_ME, REQUEST_ASKED_ME, ASKED_ME_COMPLETED,CLEAR_ASKED_ME } from  '../../actions/ActionTypes'
 const defaultState = {
     loading:false,
     data:[],
@@ -9,6 +9,8 @@ const askedMe = (state = defaultState, action) => {
     switch(action.type){
         case REQUEST_ASKED_ME:
             return Object.assign({}, state, {loading: true});
+        case CLEAR_ASKED_ME:
+            return defaultState;
         case RECEIVE_ASKED_ME:
             const data = state.data.concat(action.data);
             return Object.assign({}, state, {loading: false, data:data, page:state.page +1});
