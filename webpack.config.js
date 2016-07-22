a63687a18275b479c4f4e41a09cf0cbae5dc102b
武文齐 <wuwenqi@neotel.com.cn>
2016-07-22 14:07:15 +0800

    检测页面刷新

--- webpack.config.js
+++ webpack.config.js
@@ -59,9 +59,9 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'redux': path.resolve('node_modules',"redux/dist/redux.min.js"),
+      // 'redux': path.resolve('node_modules',"redux/dist/redux.min.js"),
       // 'react-dom': path.resolve('node_modules',"react-dom/dist/react-dom.min.js"),
-      'react-redux': path.resolve('node_modules',"react-redux/dist/react-redux.min.js")
+      // 'react-redux': path.resolve('node_modules',"react-redux/dist/react-redux.min.js")
     }
   }
 }
