41655d8ba6cdd040932fafe1b6b37bcc87605db2
wuwq <wuwenqi@neotel.com.cn>
2016-08-11 23:00:02 +0800

    改bug

--- src/javascripts/containers/lists/AskedMeList.js
+++ src/javascripts/containers/lists/AskedMeList.js
@@ -37,7 +37,7 @@ class AskedMeList extends Component {
     const {completed, page, loading} = this.props;
     let container = document.getElementsByClassName('app-container')[0];
     console.log(container.clientHeight, '+', container.scrollTop, '=', container.clientHeight + container.scrollTop, container.scrollHeight);
-    if (!loading && container.clientHeight + container.scrollTop + 1 == container.scrollHeight  && !completed) {
+    if (!loading && container.clientHeight + container.scrollTop + 1 >= container.scrollHeight  && !completed) {
       this.props.getAskedMe(page, 10);
     }
   }
