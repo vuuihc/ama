91524570bc78a3a4e93c6572d766d02635a12789
vuuihc <zshyou@gmail.com>
2016-09-06 23:51:33 +0800

    u

--- src/javascripts/api/config.js
+++ src/javascripts/api/config.js
@@ -1,3 +1,3 @@
 export const  domain = 'http://h5app.7dyk.com/ama/api/public';
-export const baseUrl = "/";
+export const baseUrl = "/ama/7dyk/";
 export const domainPath = 'http://h5app.7dyk.com/ama/7dyk/';
