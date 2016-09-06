6da5658e6a26d28cb258d60f775745c368c3b89f
vuuihc <zshyou@gmail.com>
2016-09-06 23:40:21 +0800

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,64 @@
 var path = require('path')
 var webpack = require('webpack')
 
-
-var isProduction = function () {
-  return process.env.NODE_ENV === 'production';
+var isProduction = function() {
+    return process.env.NODE_ENV.toString() == "production";
 };
 
 var plugins = [];
 
-if(isProduction()){
-  console.log('生产环境');
-  plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress:{
-          warnings: false
-        }
-      })
-  )
-}else{
-  plugins.push(
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  )
+if (isProduction()) {
+    console.log('生产环境');
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    )
+} else {
+    plugins.push(
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    )
 }
 
 module.exports = {
-  devtool: isProduction()?false:'inline-source-map',
-  entry: {
-    mobile: './src/javascripts/index.js'
-  },
-  output: {
-    path: './dist',
-    filename: '[name].js',
-    // publicPath: 'http://h5app.7dyk.com/ama/7dyk/dist/'
-    publicPath: '/dist/'
-  },
-  plugins: plugins,
+    devtool: isProduction() ? false : 'inline-source-map',
+    entry: {
+        mobile: './src/javascripts/index.js'
+    },
+    output: {
+        path: './dist',
+        filename: '[name].js',
+        publicPath: 'http://h5app.7dyk.com/ama/7dyk/dist/'
+            // publicPath: '/dist/'
+    },
+    plugins: plugins,
 
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel?presets[]=es2015&presets[]=react'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        loader: 'style!css!sass'
-      },
-	  {
-		test:/\.(png|jpg|bmp)$/,
-		loader: 'url?limit=5000&name=images/[name].[ext]'
-	  }
-    ]
-  },
-  externals: {
-    'babel-polyfill': 'true',
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-  },
-  resolve: {
-    alias: {
-      'redux': path.resolve('node_modules',"redux/dist/redux.min.js"),
-      // 'react-dom': path.resolve('node_modules',"react-dom/dist/react-dom.min.js"),
-      'react-redux': path.resolve('node_modules',"react-redux/dist/react-redux.min.js")
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel?presets[]=es2015&presets[]=react'],
+            exclude: /node_modules/,
+        }, {
+            test: /\.s?css$/,
+            loader: 'style!css!sass'
+        }, {
+            test: /\.(png|jpg|bmp)$/,
+            loader: 'url?limit=5000&name=images/[name].[ext]'
+        }]
+    },
+    externals: {
+        'babel-polyfill': 'true',
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+    },
+    resolve: {
+        alias: {
+            'redux': path.resolve('node_modules', "redux/dist/redux.min.js"),
+            // 'react-dom': path.resolve('node_modules',"react-dom/dist/react-dom.min.js"),
+            'react-redux': path.resolve('node_modules', "react-redux/dist/react-redux.min.js")
+        }
     }
-  }
 }
